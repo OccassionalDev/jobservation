2427ead50ef87166f6a17623c6f0b91887ec3dfa
OccassionalDev <ericaschimmel01@gmail.com>
2020-03-19 16:05:09 -0400

    added a way to signup, login, and access the user home, erb pages for signup, login, and the user home are created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,65 @@
+class ApplicationController < Sinatra::Base  
 
-class ApplicationController < Sinatra::Base
-    
+    configure do
+        set :public_folder, "public"
+        set :views, "app/views"
+        enable :sessions
+        set :session_secret, "jobservation"
+    end
+
     get '/' do 
         erb :index
     end 
 
+    get '/sign-up' do 
+        erb :sign_up
+    end 
+
+    post '/sign-up' do 
+        user = User.new(:name => params[:name], :email => params[:email], :password => params[:password])
+
+        if user.save 
+            session[:user_id] = user.id
+            redirect to '/login'
+        end 
+    end 
+
+    get '/login' do 
+        erb :login
+    end 
+
+    post '/login' do 
+        user = User.find_by(:email => params[:email])
+
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id 
+            redirect '/organizer'
+
+        else 
+            redirect '/login-failure'
+        end 
+    end 
+
+    get '/organizer' do 
+        erb :organizer 
+    end 
+
+    get '/login-failure' do 
+        erb :login_failure
+    end 
+
+    get '/logout' do 
+        session.clear
+        redirect to '/'
+    end 
+
+    helpers do 
+        def current_user 
+            User.find(session[:user_id])
+        end 
+
+        def logged_in?
+            !!current_user 
+        end 
+    end 
 end 
