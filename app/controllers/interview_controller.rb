851cdb02f29cbd133d4dd1110bf9026b95eff672
OccassionalDev <ericaschimmel01@gmail.com>
2020-03-19 19:50:40 -0400

    added a way to edit and remove interviews through edit_interview and edit_remove_interview_selection.erb and added needed routes for these actions in interview controller

--- app/controllers/interview_controller.rb
+++ app/controllers/interview_controller.rb
@@ -17,4 +17,34 @@ class InterviewController < ApplicationController
 
         redirect to '/organizer'
     end 
+
+    get '/organizer/edit-remove-interview' do 
+        current_user = User.find(session[:user_id])
+        @interviews = Interview.all.select { |i| i.user_id == current_user.id}
+        
+        erb :edit_remove_interview_selection
+    end 
+
+    get '/organizer/edit-remove-interview/:id' do 
+        @interview = Interview.find(params[:id])
+        erb :edit_interview 
+    end 
+
+    patch '/organizer/edit-remove-interview/:id' do
+        @interview = Interview.find(params[:id])
+
+        @interview.company = params[:company]
+        @interview.position = params[:position]
+        @interview.date = params[:date]
+        @interview.time = params[:time]
+        @interview.save 
+
+        redirect to '/organizer'
+    end 
+
+    delete '/organizer/edit-remove-interview/:id' do 
+        @interview = Interview.find(params[:id])
+        @interview.delete 
+        redirect to '/organizer'
+    end 
 end 
